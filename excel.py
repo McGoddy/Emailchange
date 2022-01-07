74514e2e8163b6b85441e9ea9c696036a6905827
McGoddy <77641241+McGoddy@users.noreply.github.com>
2022-01-08 00:19:05 +0100

    Update excel.py

--- excel.py
+++ excel.py
@@ -1,8 +1,13 @@
+# importing panda
 import pandas as pd
 
+# reading the data from the excel file
 data2 = pd.read_excel('employeedata.xlsx')
 
+# replacing the something@helpinghands.cm with something@helpinghands.org using the loc function
 data2.loc[data2['Email_Address'] == 'something@helpinghands.cm', 'Email_Address'] = 'something@helpinghands.org'
 
+# printing the modified document
 print(data2)
+# making a copy of the document so as to avoid voiding the document in case of any errors 
 data2.to_excel('modemployeedata.xlsx', index=False)
