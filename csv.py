ceb74305dd288eaac8c12c4e7e324c470d4bd08f
McGoddy <77641241+McGoddy@users.noreply.github.com>
2022-01-08 00:17:44 +0100

    Update csv.py

--- csv.py
+++ csv.py
@@ -1,10 +1,15 @@
+# importing panda
 import pandas as pd
-  
+
+# reading the data from the excel file
 data = pd.read_csv('employeedata.csv')
 
+# replacing the something@helpinghands.cm with something@helpinghands.org using the loc function
 data.loc[data['Email_Address'] == 'something@helpinghands.cm', 'Email_Address'] = 'something@helpinghands.org'
 
+# printing the modified document
 print(data)
+# making a copy of the document so as to avoid voiding the document in case of any errors 
 data.to_csv('modemployeedata.csv', index=False)
 
 
