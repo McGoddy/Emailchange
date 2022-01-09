f037dcc5bfd0e9ad2c9afbff3737144f1447962c
McGoddy <77641241+McGoddy@users.noreply.github.com>
2022-01-09 15:12:55 +0100

    Add files via upload

--- script.py
+++ script.py
@@ -1,32 +1,39 @@
+# importing openpyxl and csv
 import openpyxl as xl
 import csv
 
-exl = 'C:/Users/HP/Documents/Python Scripts/employedata.xlsx'
+exl = 'employedata.xlsx'
 
+#loading a workbook to contain the excel document
 wb = xl.load_workbook(exl)
 
+# Telling python which excel sheet we are working with
 sheet = wb['sheet1']
 
+# attributing helpinghands.cm and handsinhand.org
 old = 'helpinghands.cm'
 new = 'handsinhand.org'
 
+# replacing helpinghands.cm with handsinhand.org
 for i in range (2, sheet.max_row + 1):
     cell = sheet.cell(i, 2)
     if old in cell.value:
         updated_email = {cell.value}.replace(old, new)
         sheet.cell(i, 2).value = updated_email
     
-
-wb.save('C:/Users/HP/Documents/Python Scripts/newemployeedata.xlsx')
-
-
+# saving the edited excel document to another name to differenciate
+wb.save('newemployeedata.xlsx')
 
 
+# Now we will do same with the .csv file.
 
-op = open('C:/Users/HP/Documents/Python Scripts/employeedata.csv', 'r')
+# opening and printing out the csv file
+op = open('employeedata.csv', 'r')
 dt = csv.DictReader(op)
 print(dt)
 up_dt = []
+
+# replacing helpinghands.cm with handsinhand.org
 for r in dt:
     print(r)
     row = {'Employee_Name' : r['Employe_Name'],
@@ -36,7 +43,8 @@ for r in dt:
 print(up_dt)
 op.close
 
-op = open('C:/Users/HP/Documents/Python Scripts/newemployeedata.csv', 'w', newline = '')
+# writing the changes to a new .csv file to differenciate 
+op = open('newemployeedata.csv', 'w', newline = '')
 headers = ['Employee Name', 'Email Address', 'Phone Number']
 data = csv.DictWriter(op, delimiter=',', fieldnames= headers)
 data.writerow(dict((heads, heads) for heads in headers))
